d35c9a5d65af2f35d431b4d81b0646ebe2f47130
891855878 <891855878@qq.com>
2022-01-05 11:29:16 +0800

    profile

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,13 +1,25 @@
 <template>
-  <h2>我的</h2>
+  <div>
+    <nar-bar class="profile">
+      <div slot="center">我的</div>
+    </nar-bar>
+  </div>
 </template>
 
 <script>
+import NarBar from 'components/common/navbar/NavBar';
 export default {
+  name:'Profile',
+  components:{
+    NarBar
+  }
 
 }
 </script>
 
-<style>
-
+<style scoped>
+  .profile{
+    background-color:var(--color-tint);
+    color: white;
+  }
 </style>
